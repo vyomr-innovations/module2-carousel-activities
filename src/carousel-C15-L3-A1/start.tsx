7770c8e0262cf960b02f3c5e1c2d10eced13a5e1
mohammad asad <asadkhan41376@gmail.com>
2025-06-23 12:20:05 +0530

    Add missing tung twister in C15-L3-A1

--- src/carousel-C15-L3-A1/start.tsx
+++ src/carousel-C15-L3-A1/start.tsx
@@ -5,8 +5,8 @@ type myProps = {
 const StrartSlide = ({setIsFirstScreen}:myProps) => {
   return (
     <div className="min-h-screen flex justify-center items-center flex-col gap-10 bg-[#F8FAFC]">
-    <div className="  shadow-black shadow-md rounded-lg p-5 text-center w-[300px]">
-    <h2 className="text-3xl text-black">Tongue twisters</h2>
+    <div className="min-w-[400px]  shadow-black shadow-md rounded-lg p-5 text-center w-[300px]">
+    <h2 className="text-3xl text-black ">Tongue twisters</h2>
     </div>
     <div className="border border-black rounded-lg px-8 py-1 shadow-inner shadow-[#000000b9] bg-yellow-400 hover:scale-90">
       <h3  className="text-[30px]  cursor-pointer text-black"
--- src/carousel-C15-L3-A1/tungTwister.tsx
+++ src/carousel-C15-L3-A1/tungTwister.tsx
@@ -2,20 +2,18 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import SlideData from "@/carousel-C15-L3-A1/tungTwister.json"
+import SlideData from "@/carousel-C15-L3-A1/tungTwister.json";
 import { Navigation } from "swiper/modules";
 import "swiper/css/navigation";
 import { Swiper as SwiperClass } from "swiper";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 
-
 export default function TungTwisterSlide() {
   const swiperRef = useRef<SwiperClass | null>(null);
   const [lastSlide, setLastSlide] = useState<number>(0);
 
   const handleNext = () => {
-    
     swiperRef.current?.slideNext();
   };
 
@@ -31,7 +29,7 @@ export default function TungTwisterSlide() {
     <div className="bg-white min-h-screen flex items-center justify-center">
       <div className="w-[950px] px-2">
         <h1 className="text-center text-3xl py-4 text-black">
-        Tongue twisters
+          Tongue twisters
         </h1>
         <Swiper
           slidesPerView={1}
@@ -44,14 +42,12 @@ export default function TungTwisterSlide() {
         >
           {SlideData.map((item, index) => (
             <SwiperSlide key={index} className="border-2 rounded-lg p-2">
-    
-          <div className="flex justify-center items-center min-h-[300px]">
-        <h3 className="text-black text-2xl">{item.text}</h3>
-          </div>
-       
+              <div className="flex justify-center items-center min-h-[300px]">
+                <h3 className="text-black text-2xl">{item.text}</h3>
+              </div>
             </SwiperSlide>
           ))}
-          <div className=" py-4 flex items-center justify-around  text-black">
+          <div className=" py-4 flex items-center justify-between  text-black">
             <div
               className={` ${
                 lastSlide > 0
